6612e62b5a8f508d966eaa4c48b6681ae6acf389
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-12 22:38:30 +0200

    functions for custom normarize names, ticket no and item

--- Train model.py
+++ Train model.py
@@ -9,3 +9,16 @@ print(f"Found TF-DF {tfdf.__version__}")
 
 train_df = pd.read_csv("/kaggle/input/titanic/train.csv")
 serving_df = pd.read_csv("/kaggle/input/titanic/test.csv")
+
+train_df.head(10)
+def custom_normalize_name(x):
+    return " ".join([v.strip(",()[].\"'") for v in x.split(" ")])
+    
+def custom_ticket_number(x):
+    return x.split(" ")[-1]
+        
+def custom_ticket_item(x):
+    items = x.split(" ")
+    if len(items) == 1:
+        return "NONE"
+    return "_".join(items[0:-1])
