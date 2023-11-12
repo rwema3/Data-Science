99af38df0fa212922eef29981fca8db73ed1431b
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-12 22:37:26 +0200

    reading data

--- Train model.py
+++ Train model.py
@@ -4,3 +4,8 @@ import os
 
 import tensorflow as tf
 import tensorflow_decision_forests as tfdf
+
+print(f"Found TF-DF {tfdf.__version__}")
+
+train_df = pd.read_csv("/kaggle/input/titanic/train.csv")
+serving_df = pd.read_csv("/kaggle/input/titanic/test.csv")
